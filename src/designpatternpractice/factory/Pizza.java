e9434e13c416288b0c26bc5a00b9c79ce70a615f
AbdulHameed Mohamed <59577985+AbdulHameedMohamed@users.noreply.github.com>
2024-05-27 02:15:09 +0300

    Implement Factory Pattern

---
+++ src/designpatternpractice/factory/Pizza.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package designpatternpractice.factory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author w
+ */
+abstract public class Pizza {
+
+    String name;
+    String dough;
+    String sauce;
+    List<String> toppings = new ArrayList();
+
+    public String getName() {
+        return name;
+    }
+
+    public void prepare() {
+        System.out.println("Preparing " + name);
+    }
+
+    public void bake() {
+        System.out.println("Baking " + name);
+    }
+
+    public void cut() {
+        System.out.println("Cutting " + name);
+    }
+
+    public void box() {
+        System.out.println("Boxing " + name);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder display = new StringBuilder();
+        display.append("---- ").append(name).append(" ----\n");
+        display.append(dough).append("\n");
+        display.append(sauce).append("\n");
+        for (String topping : toppings) {
+            display.append(topping).append("\n");
+        }
+        return display.toString();
+    }
+}
---
+++ src/designpatternpractice/factory/PizzaTestDrive.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package designpatternpractice.factory;
+
+/**
+ *
+ * @author w
+ */
+public class PizzaTestDrive {
+ 
+	public static void main(String[] args) {
+		SimplePizzaFactory factory = new SimplePizzaFactory();
+		PizzaStore store = new PizzaStore(factory);
+
+		Pizza pizza = store.orderPizza("cheese");
+		System.out.println("We ordered a " + pizza.getName() + "\n");
+		System.out.println(pizza);
+ 
+		pizza = store.orderPizza("veggie");
+		System.out.println("We ordered a " + pizza.getName() + "\n");
+		System.out.println(pizza);
+	}
+}
