200c7b37c5b808e58fb53da307efda174f413af7
AbdulHameed Mohamed <59577985+AbdulHameedMohamed@users.noreply.github.com>
2024-05-28 22:35:16 +0300

    Implement Template Method Pattern

---
+++ src/designpatternpractice/template/CaffeineBeverageWithHook.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package designpatternpractice.template;
+
+/**
+ *
+ * @author w
+ */
+public abstract class CaffeineBeverageWithHook {
+
+    final void prepareRecipe() {
+        boilWater();
+        brew();
+        pourInCup();
+        if (customerWantsCondiments()) {
+            addCondiments();
+        }
+    }
+
+    abstract void brew();
+
+    abstract void addCondiments();
+
+    void boilWater() {
+        System.out.println("Boiling water");
+    }
+
+    void pourInCup() {
+        System.out.println("Pouring into cup");
+    }
+
+    boolean customerWantsCondiments() {
+        return true;
+    }
+}
