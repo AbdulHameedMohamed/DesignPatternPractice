650724e0251afc12b10aab0dc56a5f56a3e83070
AbdulHameed Mohamed <59577985+AbdulHameedMohamed@users.noreply.github.com>
2024-06-02 02:35:55 +0300

    implement simple remote with command pattern

---
+++ src/designpatternpractice/command/GarageDoorOpenCommand.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package designpatternpractice.command;
+
+/**
+ *
+ * @author w
+ */
+public class GarageDoorOpenCommand implements Command {
+
+    GarageDoor garageDoor;
+
+    public GarageDoorOpenCommand(GarageDoor garageDoor) {
+        this.garageDoor = garageDoor;
+    }
+
+    public void execute() {
+        garageDoor.up();
+    }
+}
